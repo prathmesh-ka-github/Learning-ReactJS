0210b0408b082b9264f1d23af47cbbfe6948cb3d
Prathmesh Kale <connect.prathmesh905@gmail.com>
2024-02-10 13:42:48 +0530

    learned how to create a reuseable card component with dummy text img and value

--- practice-react/src/App.jsx
+++ practice-react/src/App.jsx
@@ -1,13 +1,9 @@
-import Header from "./Header.jsx"
-import Footer from "./Footer.jsx"
-import Food from "./Food.jsx"
+import Card from "./Card.jsx"
 function App() {
 
   return (
     <>
-      <Header/>
-      <Food/>
-      <Footer/>
+      <Card/>
     </>
   )
 }
