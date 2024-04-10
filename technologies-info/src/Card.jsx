87ca9c65d09c3a587699677841728b355680229a
Prathmesh Kale <connect.prathmesh905@gmail.com>
2024-04-10 09:13:39 +0530

    minor fixes with code clean up

--- technologies-info/src/Card.jsx
+++ technologies-info/src/Card.jsx
@@ -4,27 +4,23 @@ function Card(props) {
     var logo = props.logo
     var shadow = props.shadow
 
-    var styles = {
-        // filter: `drop-shadow( 1px 1px 20px #${shadow})`
+    function MouseOver(e) {
+        e.target.style.filter = `drop-shadow( 1px 1px 20px #${shadow})`;
+    }
+    function MouseOut(e) {
+        e.target.style.filter = "drop-shadow( 1px 1px 20px #ffffff00)";
     }
 
-    function MouseOver(event) {
-        event.target.style.filter = `drop-shadow( 1px 1px 20px #${shadow})`;
-      }
-      function MouseOut(event){
-        event.target.style.filter="drop-shadow( 1px 1px 20px #ffffff00)";
-      }
-
     return (
-        <div className="card">
+        <a href={props.url} target="_blank" className="card">
             <img src={`/src/assets/${logo}`} alt={props.logo} style={styles}
-            onMouseOver={MouseOver}
-            onMouseOut={MouseOut}/>
+                onMouseOver={MouseOver}
+                onMouseOut={MouseOut} />
             <h1>
                 {props.title}
             </h1>
             <p>{props.desc}</p>
-        </div>
+        </a>
     )
 }
 
